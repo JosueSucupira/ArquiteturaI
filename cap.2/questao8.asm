989f17ce88dde7102945e085e2a6ac36463ecc25
JosueSucupira <josue.sucupirao@gmail.com>
2023-04-02 00:22:40 -0300

    atividade comcluida

---
+++ cap.2/questao8.asm
@@ -0,0 +1,28 @@
+        .686
+        .model flat, c
+        .stack 100h
+printf  PROTO arg1:Ptr byte, printlist:VARARG
+scanf   PROTO arg2:Ptr byte, inputlist:VARARG
+        .data
+msg0fmt byte "%s", 0
+in0fmt  byte "%d", 0
+msg1fmt byte 0Ah, "%s", 0
+msg2fmt byte 0Ah, "%s%d", 0
+msg3fmt byte 0Ah, "%s%d", 0Ah, 0
+msg0    byte "Enter a number: ", 0
+msg1    byte "Numbers", 0
+msg2    byte "   ", 0
+num1    sdword ?
+num2    sdword ?
+        .code
+main     proc
+        INVOKE printf, ADDR msg0fmt, ADDR msg0
+        INVOKE scanf, ADDR in0fmt, ADDR num1
+        INVOKE printf, ADDR msg0fmt, ADDR msg0
+        INVOKE scanf, ADDR in0fmt, ADDR num2
+        INVOKE printf, ADDR msg1fmt, ADDR msg1
+        INVOKE printf, ADDR msg2fmt, ADDR msg2, num1
+        INVOKE printf, ADDR msg3fmt, ADDR msg2, num2
+        ret
+main    endp
+        end
